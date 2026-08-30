f5d047b84a792aaba6419d0e8d45b6bce4cf3c51
N1TIVO-star <gustavometzher@gmail.com>
2026-08-29 21:44:48 -0300

    V3.0: carregar camada visual final

--- v28-source-patch.js
+++ v28-source-patch.js
@@ -1,6 +1,10 @@
 module.exports = function applyV28SourcePatch(source) {
-  source = source.replace('"/v25-final-ui.css"]', '"/v25-final-ui.css", "/v28-pwa.css"]');
-  source = source.replace('"/v26-stability.js"]', '"/v26-stability.js", "/v28-pwa.js"]');
-  source = source.replace('"/v25-final-ui.js", "/v26-stability.js"]', '"/v25-final-ui.js", "/v26-stability.js", "/v28-pwa.js"]');
+  source = source.replace('"/v25-final-ui.css"]', '"/v25-final-ui.css", "/v28-pwa.css", "/v30-ui.css"]');
+  source = source.replace('"/v28-pwa.css"]', '"/v28-pwa.css", "/v30-ui.css"]');
+
+  source = source.replace('"/v26-stability.js"]', '"/v26-stability.js", "/v28-pwa.js", "/v30-ui.js"]');
+  source = source.replace('"/v25-final-ui.js", "/v26-stability.js"]', '"/v25-final-ui.js", "/v26-stability.js", "/v28-pwa.js", "/v30-ui.js"]');
+  source = source.replace('"/v28-pwa.js"]', '"/v28-pwa.js", "/v30-ui.js"]');
+
   return source;
 };
